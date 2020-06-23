6cb68efaed3555cf9002b24f553a0330a14b5ab0
ajtmccarty <aaronjohntmccarty@gmail.com>
2020-06-23 00:09:53 +0000

    Add simple Node class for bfs/dfs

--- generic_search.py
+++ generic_search.py
@@ -1,4 +1,4 @@
-from typing import Any, Generic, List
+from typing import Any, Generic, List, Optional
 
 
 class Stack:
@@ -17,3 +17,20 @@ class Stack:
 
     def __repr__(self) -> str:
         return repr(self._container)
+
+
+class Node:
+    def __init__(
+        self,
+        state: Any,
+        parent: Optional["Node"],
+        cost: float = 0.0,
+        heuristic: float = 0.0,
+    ) -> None:
+        self.state: Any = state
+        self.parent: Optional["Node"] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+    def __lt__(self, other: "Node") -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
