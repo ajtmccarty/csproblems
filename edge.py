75b574761749102962b90550b02b440086dcc1e4
ajtmccarty <aaronjohntmccarty@gmail.com>
2020-06-23 20:02:25 +0000

    Add weighted graph and edges

--- edge.py
+++ edge.py
@@ -11,3 +11,17 @@ class Edge:
 
     def __str__(self) -> str:
         return f"{self.u} -> {self.v}"
+
+
+@dataclass
+class WeightedEdge(Edge):
+    weight: float
+
+    def reversed(self) -> "WeightedEdge":
+        return WeightedEdge(u=self.v, v=self.u, weight=self.weight)
+
+    def __lt__(self, other: "WeightedEdge") -> bool:
+        return self.weight < other.weight
+
+    def __str__(self) -> str:
+        return f"{self.u} {self.weight}> {self.v}"
